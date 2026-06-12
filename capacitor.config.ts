16ab07138c063ba5488fe1e1033ab0b63c2d351f
Damon Lindsay <damon.lindsay@my.jcu.edu.au>
2026-06-12 10:44:02 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meal-prep-planner',
+  webDir: 'dist'
+};
+
+export default config;
