6047ac99312c4d8fd6871a596d95b0c53a19b07b
Damon Lindsay <damon.lindsay@my.jcu.edu.au>
2026-06-12 12:15:58 +1000

    fix: restore add meal button to planner day cards

--- src/views/PlannerView.vue
+++ src/views/PlannerView.vue
@@ -77,6 +77,12 @@
             </div>
           </div>
 
+          <div class="add-row">
+            <button class="add-meal-btn" @click="openPicker(day, null)">
+              <IonIcon :icon="addCircleOutline" /> Add a meal
+            </button>
+          </div>
+
         </div>
 
         <div class="flex gap-3 mt-2">
